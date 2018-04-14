dfdd11b41845840c35d000b5b900b82c6219e8f0
Sławomir Hagiel <shprojekty@gmail.com>
2018-04-14 16:40:10 +0200

    Initial commit

---
+++ app/src/main/java/com/sh/study/udacitynano/bakingapp/recipes/RecipesActivity.java
@@ -0,0 +1,15 @@
+package com.sh.study.udacitynano.bakingapp.recipes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sh.study.udacitynano.bakingapp.R;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
---
+++ app/src/main/java/com/sh/study/udacitynano/bakingapp/recipes/RecipesActivityFragment.java
@@ -0,0 +1,24 @@
+package com.sh.study.udacitynano.bakingapp.recipes;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sh.study.udacitynano.bakingapp.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RecipesActivityFragment extends Fragment {
+
+    public RecipesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipes, container, false);
+    }
+}
